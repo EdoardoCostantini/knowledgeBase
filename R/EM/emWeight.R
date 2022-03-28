f849a64c4f6a82c8ef419d36606c6eb9a6629793
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-03-28 19:18:33 +0200

    content: second attempt at em weighted

--- R/EM/emWeight.R
+++ R/EM/emWeight.R
@@ -2,7 +2,7 @@
 # Objective: function to estimate descriptives on data w/ miss fast R style
 # Author:    Edoardo Costantini
 # Created:   2021-12-02
-# Modified:  2021-12-02
+# Modified:  2022-03-27
 # Notion:
 # Other ref:
 # Note:      This function implements the EM psuedo code for normal data
@@ -10,19 +10,42 @@
 #            Schafer 1997 (figure 5.2 under section 5.3.3), but tries
 #            to use R vectorisation to make the code faster and easier to read.
 
-emWeight <- function (Y, wt, iters, theta0){
-  ## Example Inputs
-  # Y = Y
-  # iters = 5e2
-  # theta0 = matrix(rep(NA, (p+1)^2 ), ncol = (p+1),
-  #                 dimnames = list(c("int", colnames(Y)),
-  #                                 c("int", colnames(Y))
-  #                                 ))
-  # theta0[, 1]   = c(-1, colMeans(Y, na.rm = TRUE)) # T1 CC
-  # theta0[1, ]   = c(-1, colMeans(Y, na.rm = TRUE))
-  # theta0[-1,-1] = cov(Y, use = "pairwise.complete.obs") * (n - 1)/n # T2 CC
+# Generate some data -----------------------------------------------------------
 
-  ## Preliminaries:
+  set.seed(20220327)
+  n <- 1e4
+  p <- 3
+  Y <- MASS::mvrnorm(n, rep(10, p), diag(1.5, p))
+  colnames(Y) <- paste0("y", 1:p)
+
+  # Define wegihts
+  wi <- runif(n)
+
+# Compute weighted covariance matrix -------------------------------------------
+
+  cov_wt_fu <- cov.wt(Y, wt = wi, method = "ML")
+
+# Impose missingness at random -------------------------------------------------
+
+  Y[sample(1:nrow(Y), nrow(Y)*.1), 1] <- NA
+  Y[sample(1:nrow(Y), nrow(Y)*.1), 2] <- NA
+
+# Estimate weighted covariance matrix with EM ----------------------------------
+
+# Store the theta you would have gotten with complete case analysis
+obs_cc <- rowSums(is.na(Y)) == 0
+cov_wt_cc <- cov.wt(Y[obs_cc, ], wt = wi[obs_cc], method = "ML")
+
+# > Preliminaries --------------------------------------------------------------
+
+  # Bad starting theta
+  theta0 = matrix(rep(NA, (p+1)^2 ), ncol = (p+1),
+                  dimnames = list(c("int", colnames(Y)),
+                                  c("int", colnames(Y))
+                  ))
+  theta0[, 1]   = c(-1, rep(5, p)) # T1 CC
+  theta0[1, ]   = c(-1, rep(5, p))
+  theta0[-1,-1] = diag(p)
 
   # Define Missing data patterns
   patts <- mice::md.pattern(Y, plot = FALSE)
@@ -34,10 +57,12 @@ emWeight <- function (Y, wt, iters, theta0){
 
   # Number of missing data patterns
   S <- nrow(R)
+
   # Columns observed for a given pattern
   O <- apply(R, 1, function(x) {colnames(R)[x == 1]})
+
   # Columns missings for a given pattern
-  M <- apply(R, 1, function(x) {colnames(R)[x == 0]}) #
+  M <- apply(R, 1, function(x) {colnames(R)[x == 0]})
 
   # Define I matrices (which obs in which pattern)
   ry <- !is.na(Y)
@@ -58,100 +83,103 @@ emWeight <- function (Y, wt, iters, theta0){
   # Define sufficient statistics matrix (observed)
   Tobs_s <- vector("list", S)
 
-  # Normalise weights (to sum to 1)
-  wt <- wt / sum(wt)
+  for(s in 1:S){
+    # Define what you are working with in this miss patt
+    Y_s <- Y[I[[s]], , drop = FALSE]
+    wi_s <- wi[I[[s]]]
 
-  # Center on weighted mean if required
-  center <- colSums(wt * x)
+    # Weigthed augmented design matrix
+    dat_aug <- as.matrix(sqrt(wi_s) * cbind(1, Y_s))
 
-  # Weight Y
-  Y <- sqrt(wt) * sweep(Y, 2, center, check.margin = FALSE)
+    # Obtain matrix of sufficient statistics (Tobs) w/ cross-product shortcut
+    Tobs_s[[s]] <- t(dat_aug) %*% dat_aug
 
-
-  for (s in 1:S) {
-    id_obs  <- I[[s]]
-    dat_aug <- as.matrix(cbind(int = 1, Y[id_obs, , drop = FALSE]))
-    Tobs_s[[s]] <- crossprod(dat_aug)
-
-    # Fix NAs
+    # Replace NAs with 0 contributions
     Tobs_s[[s]][is.na(Tobs_s[[s]])] <- 0
   }
 
   Tobs <- Reduce("+", Tobs_s)
+  dimnames(Tobs) <- dimnames(theta0)
 
-  ## EM algorithm
+# > EM algorithm ---------------------------------------------------------------
+
   # Starting values
   theta <- theta0
+  iters <- 500
+  ith_theta <- NULL
 
   # Iterations
   for(it in 1:iters){
+    print(it)
+
     # Reset T to info in the data
-    # (will be updated based on new theta at every new iteration)
-    #> E-step ####
-    T <- Tobs
-    if(S > 1){
-      # We only need to do this if there are missing data patterns other than
-      # the fully observed pattern (s = 1)
-      for(s in 2:S){
-        # Description: For every missing data patter, except the first one (complete data
-        # missing data pattern)
-        # s <- 3
-        obs   <- I[[s]]
-        v_obs <- O[[s]]
-        v_mis <- M[[s]]
-        v_all <- colnames(Y)
+    Tmat <- Tobs
 
-        # Sweep theta over predictors for this missing data pattern
-        theta <- ISR3::SWP(theta, v_obs)
+    # > E step -----------------------------------------------------------------
 
-        # Define expectations (individual contributions)
-        betas <- theta[c("int", v_obs), v_mis]
-        cjs <- cbind(1, Y[obs, v_obs, drop = FALSE]) %*% betas
+    for(s in 2:S){
+      # For every missing data patter, add exceptation
+      # s <- 2
+      obs   <- I[[s]]
+      v_obs <- O[[s]]
+      v_mis <- M[[s]]
+      v_all <- colnames(Y)
 
-        # Update T matrix ##
-        for(i in seq_along(obs)){
-          for(j in seq_along( v_mis ) ){
-            # j <- 1
-            # Update for mean
-            J <- which(v_all == v_mis[j])
-            T[1, J+1] <- T[1, J+1] + cjs[i, j]
-            T[J+1, 1] <- T[1, J+1]
+      # Sweep theta over predictors for this missing data pattern
+      theta <- ISR3::SWP(theta, v_obs)
 
-            # Update for covariances w/ observed covariates for this id
-            # (for Ks observed for this id)
-            for(k in seq_along( v_obs )){
-              # k <- 1
-              K <- which(v_all == v_obs[k])
-              T[K+1, J+1] <- T[K+1, J+1] + cjs[i, j] * Y[obs[i], K]
-              T[J+1, K+1] <- T[K+1, J+1]
-            }
+      # Define expectations (individual contributions)
+      betas <- theta[c("int", v_obs), v_mis]
+      cjs <- cbind(1, Y[obs, v_obs, drop = FALSE]) %*% betas
 
-            # Update for covariances w/ unobserved covariates for this id
-            # (both j and k missing, includes covariances with itself k = j)
-            for(k in seq_along( v_mis )){
-              # k <- 1
-              K <- which(v_all == v_mis[k])
-              if(K >= J){
-                T[K+1, J+1] <- T[K+1, J+1] + theta[K+1, J+1] + cjs[i, j] * cjs[i, k]
-                T[J+1, K+1] <- T[K+1, J+1]
-              }
+      # Update Tmat matrix ##
+      for(i in seq_along(obs)){
+        # i <- 1
+        for(j in seq_along( v_mis ) ){
+          # j <- 1
+          # Update for mean
+          J <- which(v_all == v_mis[j])
+          Tmat[1, J+1] <- Tmat[1, J+1] + cjs[i, j] * wi[obs[i]]
+          Tmat[J+1, 1] <- Tmat[1, J+1]
+
+          # Update for covariances w/ observed covariates for this id
+          # (for Ks observed for this id)
+          for(k in seq_along( v_obs )){
+            # k <- 1
+            K <- which(v_all == v_obs[k])
+            Tmat[K+1, J+1] <- Tmat[K+1, J+1] + cjs[i, j] * Y[obs[i], K] * wi[obs[i]]
+            Tmat[J+1, K+1] <- Tmat[K+1, J+1]
+          }
+
+          # Update for covariances w/ unobserved covariates for this id
+          # (both j and k missing, includes covariances with itself k = j)
+          for(k in seq_along( v_mis )){
+            # k <- 1
+            K <- which(v_all == v_mis[k])
+            if(K >= J){
+              Tmat[K+1, J+1] <- Tmat[K+1, J+1] + theta[K+1, J+1] + cjs[i, j] * cjs[i, k] * wi[obs[i]]
+              Tmat[J+1, K+1] <- Tmat[K+1, J+1]
             }
           }
         }
-        theta <- ISR3::RSWP(theta, v_obs)
-        # Note: this corresponds to the reverse sweep in the first
-        # loop performed in the algorithm proposed by Schafer 1997.
-        # It basically replaces the "if r_sj = 0 and theta_jj < 0".
-        # For one E step, the covariance matrix used to compute individual
-        # contirbutions in each missing data pattern is the same!
       }
+      theta <- ISR3::RSWP(theta, v_obs)
     }
 
-    # > M-step ####
-    theta <- SWP((n^(-1) * T), 1)
+    # > M step -----------------------------------------------------------------
 
+    ith_theta[[it]] <- ISR3::SWP((sum(wi)^(-1) * Tmat), 1)
+    theta <- ith_theta[[it]]
   }
 
-  return(theta)
+# Covariance matrix
+data.frame(CC = c(cov_wt_cc$cov),
+           FULL = c(cov_wt_fu$cov),
+           EM = c(theta[-1, -1])
+)
 
-}
+# Vector of centers
+data.frame(CC = cov_wt_cc$center,
+           FULL = cov_wt_fu$center,
+           EM = theta[-1, 1]
+)
